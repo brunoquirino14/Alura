f21049662962a6ff89944deb2d00e8630bfa2991
brunoquirino14 <brunoquirino@outlook.com.br>
2023-08-27 12:01:11 -0300

    Adicionando novo projeto da alura.

---
+++ Java - Criando a sua primeira aplicacao/Exercicio1/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0)/3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = "Filme de aventura dos anos 80";
+        System.out.println(sinopse);
+        System.out.println();
+
+    }
+}
